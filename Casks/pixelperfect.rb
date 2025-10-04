12bd65d6eb142148c2bc5d7f5e665fd926ebbc3a
János Orcsik <4984853+janosorcsik@users.noreply.github.com>
2025-10-04 17:20:02 +0200

    Add pixelperfect

---
+++ Casks/pixelperfect.rb
@@ -0,0 +1,17 @@
+cask "pixelperfect" do
+  version "1.4"
+  sha256 "31b98c0d3afc71df6e4ea99be82c89d3d910ca7dbb2c6d553254e4ac86f25ece"
+
+  url "https://github.com/cormiertyshawn895/PixelPerfect/releases/download/#{version}/PixelPerfect.#{version}.zip"
+  name "PixelPerfect"
+  desc "ncrease the text size of iPhone and iPad apps on Mac."
+  homepage "https://github.com/cormiertyshawn895/PixelPerfect/"
+
+  app "PixelPerfect.app"
+
+  zap trash: [
+    "~/Library/Caches/com.mac.PixelPerfect",
+    "~/Library/HTTPStorages/com.mac.PixelPerfect",
+    "~/Library/Preferences/com.mac.PixelPerfect.plist",
+  ]
+end
